d10753fc5344018c691e65b5b5eae4c348599d2e
DizzyDogg <jeffthetenor@yahoo.com>
2014-08-20 14:12:58 -0600

    modified the README to be wilderness, not dungeon

---
+++ Item/Template.pm
@@ -0,0 +1,20 @@
+package Item::;
+
+use strict;
+use warnings;
+
+use base qw(Item);
+
+sub _desc {
+    return "";
+}
+
+sub get_ingredients {  }
+
+sub get_tools {  }
+
+sub process {
+    return "";
+}
+
+1;
