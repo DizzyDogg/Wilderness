79dedbc891c7f08535c98f18aabb18978169f72d
DizzyDogg <jeffthetenor@yahoo.com>
2014-08-29 11:00:15 -0600

    Changed the way Deer spawn

--- Mob/Deer.pm
+++ Mob/Deer.pm
@@ -4,7 +4,17 @@ use strict;
 use warnings;
 
 use base qw(Mob);
+use Item::Hide;
+use Item::Venison;
 
 sub _desc { "The deer seems cute and kind ... but the sight of him makes you hungry" }
+
+sub initialize {
+    my $self = shift;
+    my $hide = Item::Hide->new();
+    $self->inventory_add($hide);
+    my $venison = Item::Venison->new();
+    $self->inventory_add($venison);
+}
 
 1;
