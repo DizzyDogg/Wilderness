8763f17ac0be0ce3f912702de16ef1c8083e958a
DizzyDogg <jeffthetenor@yahoo.com>
2014-09-04 11:38:26 -0600

    oops, that initialize is not needed, it is in "Place" already

--- Biome.pm
+++ Biome.pm
@@ -7,11 +7,4 @@ use base qw(Place);
 
 sub is_biome { return 1 }
 
-sub initialize {
-    my $self = shift;
-    $self->SUPER::initialize();
-    delete $self->{'hidden'};
-    return $self;
-}
-
 1;
